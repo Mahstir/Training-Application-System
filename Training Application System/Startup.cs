a8440854419d7dfbb6359c696efac539ef18f824
Faboro <Faboro@MAHSTIR>
2021-10-20 02:52:25 +0100

    Add project files.

---
+++ Training Application System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Training_Application_System.Startup))]
+namespace Training_Application_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
