1312857d60160fc380e8ee7bc85c977068b207b1
Mahstir <maxter.fa@gmail.com>
2021-10-20 03:58:55 +0100

    Initial commit containing the Attendee Model, Controller and View

--- Training Application System/Models/IdentityModels.cs
+++ Training Application System/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace Training_Application_System.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Attendee> Attendees { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
