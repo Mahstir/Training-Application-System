9f4483ba9ddd836065d4fb70d8d953c5e8b670ca
Mahstir <maxter.fa@gmail.com>
2021-10-21 10:23:22 +0100

    Changes to Controllers

---
+++ Training Application System/Controllers/AttendeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Training_Application_System.Models;
+
+namespace Training_Application_System.Controllers
+{
+    public class AttendeeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AttendeeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+    }
+}
+       
